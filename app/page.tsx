4949a2112b268c34e8412d3ce74934b066e34eae
Luis Gonzalez <lgonzalez23@verizon.net>
2023-10-02 19:29:23 -0700

    navbar logo

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <h1>Watchful Price</h1>
+      <h1>main body</h1>
     </main>
   );
 }
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,7 +1,29 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <header className='w-full'>
+      <nav className='nav'>
+        <Link
+          href={"/"}
+          className='flex items-center gap-1'
+        >
+          <Image
+            src={"assets/icons/logo.svg"}
+            alt='watchful price logo'
+            width={27}
+            height={27}
+          />
+          <p className='nav-logo'>
+            Watchful{" "}
+            <span className='text-primary'>Price</span>
+          </p>
+        </Link>
+      </nav>
+    </header>
+  );
 };
 
 export default NavBar;
