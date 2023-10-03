a9b2f9021e47f19f177b486e00a9d295e7d3d070
Luis Gonzalez <lgonzalez23@verizon.net>
2023-10-02 19:31:20 -0700

    navIcons constant

---
+++ constants/index.ts
@@ -0,0 +1,14 @@
+export const navIcons = [
+  {
+    src: "/assets/icons/search.svg",
+    alt: "search",
+  },
+  {
+    src: "/assets/icons/black-heart.svg",
+    alt: "heart",
+  },
+  {
+    src: "/assets/icons/user.svg",
+    alt: "user",
+  },
+];
