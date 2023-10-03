65d5fef17f5265181a272995fe9878fab4459823
Luis Gonzalez <lgonzalez23@verizon.net>
2023-10-02 19:42:11 -0700

    navbar icons

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { navIcons } from "@/constants";
 
 const NavBar = () => {
   return (
@@ -21,6 +22,26 @@ const NavBar = () => {
             <span className='text-primary'>Price</span>
           </p>
         </Link>
+        <div>
+          <ul className='flex items-center gap-5'>
+            {navIcons.map(({ src, alt }) => {
+              return (
+                <li
+                  key={alt}
+                  className='p-2 rounded-full hover:bg-slate-100 cursor-pointer'
+                >
+                  <Image
+                    src={src}
+                    alt={alt}
+                    width={28}
+                    height={28}
+                    className='object-contain'
+                  />
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </nav>
     </header>
   );
