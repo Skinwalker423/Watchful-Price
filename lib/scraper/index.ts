5c226487b27c3084eff602628b8e59fed7309c17
Luis Gonzalez <lgonzalez23@verizon.net>
2023-10-05 20:41:49 -0700

    updated extractPrice func

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -2,6 +2,7 @@
 
 import axios from "axios";
 import * as cheerio from "cheerio";
+import { extractPrice } from "../utils";
 
 export async function scrapeAmazonProduct(url: string) {
   if (!url) return;
@@ -24,16 +25,24 @@ export async function scrapeAmazonProduct(url: string) {
 
   try {
     const response = await axios.get(url, options);
-    console.log(
-      "this is the response data:",
-      response.data
-    );
 
     const $ = cheerio.load(response.data);
     const title = $("#productTitle").text().trim();
     const price = $("span.a-price-whole").text().trim();
 
-    console.log("title and price of macbook", title, price);
+    const currentPrice = extractPrice(
+      $("#attach-base-product-price"),
+      $(".a-price.a-text-price"),
+      $(".priceToPay span.a-offscreen"),
+      $("a.size.base.a-color-price"),
+      $(".a-button-selected .a-color-base")
+    );
+
+    console.log(
+      "title and price of macbook",
+      title,
+      currentPrice
+    );
   } catch (error: any) {
     throw new Error(
       `failed to scrape product: ${error.message}`
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,8 +1,17 @@
 export function extractPrice(...elements: any) {
   for (const element of elements) {
     const priceText = element.text().trim();
+
     if (priceText) {
-      return priceText.replace(/\D/g, "");
+      const cleanPrice = priceText.replace(/[^\d.]/g, "");
+
+      let firstPrice;
+
+      if (cleanPrice) {
+        firstPrice = cleanPrice.match(/\d+\.\d{2}/)?.[0];
+      }
+
+      return firstPrice || cleanPrice;
     }
   }
 
